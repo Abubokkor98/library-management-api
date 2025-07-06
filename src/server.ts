47ef99a234f175029cdad496f3326a7453e4df17
Abubokkor98 <mail.abubokkor@gmail.com>
2025-07-06 13:03:22 +0600

    feat: implement book and borrow management features with CRUD operations

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import config from "./config";
+import routes from "./routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+ app.use(routes);
+
 
 app.listen(config.port, () => {
   console.log(` server is running on port ${config.port}`);
